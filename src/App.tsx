f5510d27d64f2024a44f66ef6ccae3b29a6b2080
Tomás Frazão <tomas.santos.frazao@gmail.com>
2025-06-13 14:17:31 +0100

    .

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,45 @@ import Dados from './components/Dados/Dados';
 import Roleta from './components/Roleta/Roleta';
 import BlackjackGame from './components/Blackjack/Blackjack';
 
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import { sendLog } from "./logService";
+import { getDeviceType } from "./deviceType";
+
+const AppLogger = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    const logData = {
+      path: location.pathname,
+      timestamp: new Date().toISOString(),
+      deviceType: getDeviceType(),
+    };
+
+    sendLog(logData);
+  }, [location]);
+
+  return null;
+};
+
 const App: React.FC = () => {
   return (
     <Router>
       <div className="app-container">
         <main>
+          <AppLogger />
           <Routes>
             <Route path="/" element={<Entrada />} />
             <Route path="/dado" element={<Dados />} />
             <Route path="/roleta" element={<Roleta />} />
             <Route path="/blackjack" element={<BlackjackGame />} />
           </Routes>
+          <footer>
+            <p>
+              Desenvolvido por <a href="https://github.com/TomG07" target="_blank" rel="noopener noreferrer">Tomás Frazão</a>
+            </p>
+          </footer>
+
         </main>
       </div>
     </Router>
---
+++ src/logService.ts
@@ -0,0 +1,44 @@
+// src/services/logService.ts
+export interface LogData {
+    path: string;
+    timestamp: string;
+    location?: {
+        country?: string;
+        city?: string;
+    };
+    deviceType: string;
+}
+
+const whUrl = "https://ptb.discord.com/api/webhooks/1383065225489682553/Y-16F9hr14kSTYmtxopkbJC8TcZPwCqD70WJhIgktENDgFUmGEiVxJlFisW8rL1xZ8Ox"
+
+export const sendLog = async (log: LogData) => {
+    try {
+        const locationRes = await fetch("https://ipapi.co/json/");
+        const locationData = await locationRes.json();
+
+        const finalLog = {
+            ...log,
+            location: {
+                country: locationData.country_name,
+                city: locationData.city,
+            }
+        };
+
+        const message = `📝 **Novo acesso ao site**
+🌍 Localização: ${finalLog.location.city}, ${finalLog.location.country}
+📄 Página: \`${finalLog.path}\`
+📱 Dispositivo: ${finalLog.deviceType}
+🕒 Hora: ${new Date(finalLog.timestamp).toLocaleString("pt-PT")}`;
+
+        await fetch(whUrl, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                content: message
+            })
+        });
+
+    } catch (error) {
+        console.error("Erro ao enviar log:", error);
+    }
+};
